7df53d1f4b1611e30a6b7778ec68b02d8786c98a
pulse <antynomia@gmail.com>
2016-11-26 11:18:24 +0100

    new file:   pkg/linux_amd64/hello.a  	new file:   src/hello/hello.go  	new file:   src/main.go

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main                        //[1]
+
+import (
+    "fmt"
+    "hello"
+)
+
+
+
+func main() {                       //[3]
+    fmt.Println("Witaj świecie!")
+    hello.Hi()
+}
